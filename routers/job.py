c5d8c054f485c6962cf7f8150fdd1fda4ad78508
dheemant <dheemantm05@gmail.com>
2026-06-26 16:49:51 +0530

    routers,schemas,postgres,api

--- routers/job.py
+++ routers/job.py
@@ -1,13 +1,31 @@
 from fastapi import APIRouter
+from schemas.job import JobCreate,JobUpdate
 
 router = APIRouter(prefix="/job", tags=["job"])
+jobs = []
+
+@router.post("/")
+def create_job(job: JobCreate):
+    jobs.append(job)
+    return {"Job": job}
+
 
 @router.get("/")
 def read_job():
-    return {"job": "Job root"}
+    return jobs
 
 @router.get("/{job_id}")
-def read_job(job_id: int):
-    return {"job_id": job_id}
+def get_job(job_id: int):
+    return jobs[job_id]
+
+@router.put("/{job_id}")
+def update_jobUpdate(job_id: int,job: JobUpdate):
+   jobs[job_id] = job
+   return jobs
+
+@router.delete("/{job_id}")
+def delete_job(job_id: int):
+   jobs.pop(job_id)
+   return jobs
 
 
