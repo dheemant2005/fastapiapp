148b597baff16b67eb273ba8a5a16b277f13ca52
dheemant <dheemantm05@gmail.com>
2026-06-30 12:39:33 +0530

    usestate and useeffect

--- talentspark/src/components/Welcome.tsx
+++ talentspark/src/components/Welcome.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 
-function Welcome(){
+
+
+function Counter(){
+    const [count, setCount] = useState(0);
+    const increment = () =>{
+        setCount(count + 1);
+    }
     return(
-        <h1>Welcome to Talentspark</h1>
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={increment}>Increment</button>
+        </div>
+        
     )
 }
 
-export default Welcome
+export default Counter
